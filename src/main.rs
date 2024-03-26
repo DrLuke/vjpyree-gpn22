18e160e4d9d9fbc630344145a3b12cad1fd6fb7b
DrLuke <Lukas@Jackowski.de>
2024-03-26 21:53:02 +0100

    Add rapier

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,16 @@ mod hexagon;
 mod eyes;
 
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
+use crate::eyes::EyesPlugin;
 use crate::hexagon::HexagonPlugin;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_plugins(HexagonPlugin)
+        .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugins(RapierDebugRenderPlugin::default())
+        //.add_plugins(HexagonPlugin)
+        .add_plugins(EyesPlugin)
         .run();
 }
