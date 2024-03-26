5e17a3c5b47214eda4b5761a27ebfa6c2c620d7f
DrLuke <Lukas@Jackowski.de>
2024-03-26 22:35:09 +0100

    Add world inspector

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod hexagon;
 mod eyes;
 
 use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use bevy_rapier3d::prelude::*;
 use crate::eyes::EyesPlugin;
 use crate::hexagon::HexagonPlugin;
@@ -9,6 +10,7 @@ use crate::hexagon::HexagonPlugin;
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(WorldInspectorPlugin::new())
         .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
         .add_plugins(RapierDebugRenderPlugin::default())
         //.add_plugins(HexagonPlugin)
