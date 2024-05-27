db095661ab5460c15c89efbbeca65cf5948bf8c7
DrLuke <Lukas@Jackowski.de>
2024-05-27 22:52:32 +0200

    Improve eyes anims

--- src/physics_hexagon/effectors/eyes_mode.rs
+++ src/physics_hexagon/effectors/eyes_mode.rs
@@ -1,4 +1,5 @@
 use std::cmp::PartialEq;
+use std::f32::consts::PI;
 use bevy::asset::Handle;
 use bevy::ecs::bundle::DynamicBundle;
 use bevy::hierarchy::Children;
@@ -25,7 +26,7 @@ pub fn eyes_mode(
         let mut rng = thread_rng();
         *beat_rot = Quat::from_axis_angle(
             Vec3::new(rng.gen::<f32>() * 2. - 1., rng.gen::<f32>() * 2. - 1., rng.gen::<f32>() * 2. - 1.).normalize(),
-            rng.gen::<f32>() * 0.02 + 0.02
+            rng.gen::<f32>() * PI - PI/2.
         );
     }
 
@@ -33,28 +34,26 @@ pub fn eyes_mode(
         for child in children {
             let mut t = model_query.get_mut(*child).unwrap();
 
-            match settings.eyes_mode {
-                EyesMode::None => { t.rotation = Quat::default(); }
-                EyesMode::Crazy => { t.rotation = t.rotation * *beat_rot; }
-                EyesMode::Stare => {
-                    let current = t.rotation;
-                    let target = p_t.to_scale_rotation_translation().1.inverse();
-                    let diff_quat = target * current.inverse();
-                    let diff_angle = target.angle_between(current);
-                    if diff_angle < 0.01 {
-                        continue;
-                    }
+            let target = match settings.eyes_mode {
+                EyesMode::None => { Quat::default() }
+                EyesMode::Crazy => { t.rotation * *beat_rot }
+                EyesMode::Stare => { p_t.to_scale_rotation_translation().1.inverse() }
+            };
 
-                    let u_diff = (diff_angle) * (time.delta_seconds()/(0.5+time.delta_seconds()));
+            let current = t.rotation;
+            let diff_quat = target * current.inverse();
+            let diff_angle = target.angle_between(current);
+            if diff_angle < 0.01 {
+                continue;
+            }
 
-                    let lerp_fac = u_diff/diff_angle;
-                    if lerp_fac.abs() < 0.01 {
-                        t.rotation = target;
-                    } else {
-                        t.rotation = t.rotation.lerp(diff_quat * current, u_diff/diff_angle);
-                    }
+            let u_diff = (diff_angle) * (time.delta_seconds()/(0.5+time.delta_seconds()));
 
-                }
+            let lerp_fac = u_diff/diff_angle;
+            if lerp_fac.abs() < 0.01 {
+                t.rotation = target;
+            } else {
+                t.rotation = t.rotation.lerp(diff_quat * current, u_diff/diff_angle);
             }
         }
     }
